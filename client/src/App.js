0d050e789c623a1f5847291696595bee31859dce
Arshad-06 <arshadhm200@gmail.com>
2023-03-17 18:56:22 +0530

    Form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,68 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { storage } from "./firebase";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+const Login = () => {
   const [imageUpload, setImageUpload] = useState(null);
   const uploadImage = () => {
     if (imageUpload == null) return;
     const imageRef = ref(storage, `images/${imageUpload.name}`);
     uploadBytes(imageRef, imageUpload).then(() => {
       alert("Image Uploaded!");
-    });
-    getDownloadURL(imageRef).then((url) => {
-      console.log(url);
+      getDownloadURL(imageRef).then((url) => {
+        console.log(url);
+      });
     });
   };
 
   return (
-    <div className="App">
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload Image</button>
+    <div className="login-outer">
+      <Form className="login">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicid">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="id" placeholder="Enter ID" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicfile">
+          <Form.Control
+            type="file"
+            onChange={(event) => {
+              setImageUpload(event.target.files[0]);
+            }}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onClick={uploadImage}>
+          Upload
+        </Button>
+      </Form>
     </div>
   );
-}
-
+};
 export default App;
