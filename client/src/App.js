da048ec2ca3513cb7ffa836b0c725955addddaa4
nikhil bhimrao babar <nikhilbabarjee@gmail.com>
2023-03-17 18:23:53 +0530

    merged arshad pull request

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { storage } from "./firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import FaceRecognition from './components/FaceRecognition';
 
 function App() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded!");
+    });
+    getDownloadURL(imageRef).then((url) => {
+      console.log(url);
+    });
+  };
+
   return (
-    <>
-        <main className='App'>
-          <FaceRecognition />
-        </main>
-    </>
+    <div className="App">
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadImage}>Upload Image</button>
+    </div>
   );
 }
 
